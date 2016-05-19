e2ef216926a0207262ccd1b3396b1fad9b5eef5b
yunshen <findsomeoneyysxxx@gmail.com>
2016-05-19 23:02:07 +0800

    #4 初始版本 页面链接跟进规则以及后续处理需要进一步分析

--- src/spider/NameCrawlerSpider/pipelines.py
+++ src/spider/NameCrawlerSpider/pipelines.py
@@ -4,8 +4,30 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
-
+from scrapy.exceptions import DropItem
 
 class NamecrawlerspiderPipeline(object):
     def process_item(self, item, spider):
+        #如果article不存在数据则丢弃
+        vaild = True
+        if not 'article' in item.keys():
+            vaild = False
+            #raise DropItem("Missing article from %s" %item['url'])
+        if vaild:
+            #把title article内的数据编码并输出
+            with open('/Users/yangyunshen/name-crawler-python/src/spider/item.json', 'a') as f:
+                f.write(item['date'])
+                f.write('\n')
+                f.write('title:')
+                for text in item['title']:
+                    f.write(text.encode('utf-8'))
+                f.write('\n')
+
+                f.write('article:')
+                for text in item['article']:
+                    f.writelines(text.encode('utf-8'))
+                f.write('\n')
+            f.close()
+
+        print ' %s 数据通过管道 ' %item['date']
         return item
--- src/spider/NameCrawlerSpider/spiders/tecentNews_spider.py
+++ src/spider/NameCrawlerSpider/spiders/tecentNews_spider.py
@@ -1,32 +1,31 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from scrapy.spider import CrawlSpider, Rule
+from scrapy.linkextractor import LinkExtractor
 
 from NameCrawlerSpider.items import NamecrawlerspiderItem
 
-class TecentNewsSpider(scrapy.spiders.Spider):
+class TecentNewsSpider(CrawlSpider):
     name = "tecent"
     allowed_domains = ["news.qq.com"]
-
     #启动时进行爬取的url列表.
     #后续的URL从初始的URL获取到得数据提取
-    start_urls = ["http://news.qq.com/a/20160517/030099.htm"]
+    start_urls = ["http://news.qq.com/"]
+
+    rules = [
+        Rule(LinkExtractor(allow=(r'http://news.*')), callback='parse_item')
+    ]
 
 
     #每个初始url完成下载后生成的Response对象将会作为唯一参数传递给该函数,此方法负责解析返回的数据
     #提取数据(生成Item)以及生成需要进一步处理的URL的Requst对象
-    def parse(self, response):
+    def parse_item(self, response):
         item = NamecrawlerspiderItem()
         item['date'] = response.url.split("/")[-2]
-        item['title'] = response.selector.xpath('//title').extract()
-        item['article'] = response.selector.xpath("//div[@id='Cnt-Main-Article-QQ']/p").extract()
+        item['title'] = response.selector.xpath('//title/text()').extract()
 
-        #如果直接输出内容为uncoide字符无法阅读 临时处理一下
-        filename = response.url.split("/")[-2]
-        with open(filename, 'wb') as f:
-            for title in item['title']:
-                f.writelines(title)
+        for text in response.selector.xpath("//div[@id='Cnt-Main-Article-QQ']/p/text()").extract():
+            item['article'] = text
 
-            for article in item['article']:
-                f.writelines(article)
-
-            f.close()
+        #print item['date']
+        return item
