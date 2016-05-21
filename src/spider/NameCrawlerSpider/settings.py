29c91aea593e1f3de5ebc59957f4259aaeb680c3
yunshen <findsomeoneyysxxx@gmail.com>
2016-05-22 00:21:49 +0800

    "5-22 抓取数据提取修改"

--- src/spider/NameCrawlerSpider/settings.py
+++ src/spider/NameCrawlerSpider/settings.py
@@ -15,6 +15,7 @@ SPIDER_MODULES = ['NameCrawlerSpider.spiders']
 NEWSPIDER_MODULE = 'NameCrawlerSpider.spiders'
 
 
+
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'NameCrawlerSpider (+http://www.yourdomain.com)'
 
@@ -24,7 +25,7 @@ NEWSPIDER_MODULE = 'NameCrawlerSpider.spiders'
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY=3
+DOWNLOAD_DELAY=3
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN=16
 #CONCURRENT_REQUESTS_PER_IP=16
@@ -61,9 +62,9 @@ NEWSPIDER_MODULE = 'NameCrawlerSpider.spiders'
 
 # Configure item pipelines
 # See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'NameCrawlerSpider.pipelines.SomePipeline': 300,
-#}
+ITEM_PIPELINES = {
+   'NameCrawlerSpider.pipelines.NamecrawlerspiderPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See http://doc.scrapy.org/en/latest/topics/autothrottle.html
