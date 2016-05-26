3d2d05d4ebfc1132939a92a9e80851a049bf37a6
yunshen <findsomeoneyysxxx@gmail.com>
2016-05-26 15:52:24 +0800

    修改processdata为一个类 现在可以在别的代码段中直接引用

--- src/spider/NameCrawlerSpider/processData.py
+++ src/spider/NameCrawlerSpider/processData.py
@@ -2,54 +2,76 @@
 import urllib
 import urllib2
 import re
+import traceback
 from jpype import *
 
-#初始化Hanlp
-startJVM(getDefaultJVMPath(), "-Djava.class.path=./hanlp-1.2.9.jar:./", "-Xms1g", "-Xmx1g")
-HanLP = JClass('com.hankcs.hanlp.HanLP')
 
-#存放提取结果的字典
-result = {
-    'name':[],
-    'address':[],
-}
+class ProcessData(object):
+    def __doc__(self):
+        """
+        这个类是调用Halp语言包来对传入的数据进行处理取出名字和地址.
+        """
 
-testCases = [
-    "签约仪式前，秦光荣、李纪恒、仇和等一同会见了参加签约的企业家。",
-    "王国强、高峰、汪洋、张朝阳光着头、韩寒、小四",
-    "张浩和胡健康复员回家了",
-    "王总和小丽结婚了",
-    "编剧邵钧林和稽道青说",
-    "这里有关天培的有关事迹",
-    "龚学平等领导,邓颖超生前",
-    "武胜县新学乡政府大楼门前锣鼓喧天",
-    "蓝翔给宁夏固原市彭阳县红河镇黑牛沟村捐赠了挖掘机",
-]
+    def __init__(self, sentence):
+        #传进来的sentence应该为一个列表
+        if type(sentence) == list:
+            self.cases = sentence
+            # 存放提取结果的字典
+            self.result = {
+                'name': [],
+                'address': [],
+            }
+            #添加提取规则
+            self.name_compiled_rule = re.compile(r'.*?(?=/nr)')
+            self.address_compiled_rule = re.compile(r'.*?(?=/ns)')
 
-#名字识别接口
-StandradTokenizer = JClass('com.hankcs.hanlp.tokenizer.StandardTokenizer')
 
-#提取数据的匹配规则
-name_rule = r'.*?(?=/nr)'
-address_rule = r'.*?(?=/ns)'
-name_compiled_rule = re.compile(name_rule)
-address_compiled_rule = re.compile(address_rule)
 
-for sentence in testCases:
-    for case in StandradTokenizer.segment(sentence):
-        # print str(case)
-        hasName = name_compiled_rule.match(str(case))
-        hasAddress = address_compiled_rule.match(str(case))
-        if hasName:
-            #print hasName.group().strip()
-            result['name'].append(hasName.group().strip())
-        if hasAddress:
-            #print hasAddress.group().strip()
-            result['address'].append(hasAddress.group().strip())
+    def process_data(self):
+        try:
+            # 初始化Hanlp
+            startJVM(getDefaultJVMPath(), "-Djava.class.path=./hanlp-1.2.9.jar:./", "-Xms1g", "-Xmx1g")
+            # 名字识别接口
+            StandradTokenizer = JClass('com.hankcs.hanlp.tokenizer.StandardTokenizer')
+        except:
+            traceback.print_exc()
+        else:
+            for sentence in self.cases:
+                for case in StandradTokenizer.segment(sentence):
+                    # print str(case)
+                    hasName = self.name_compiled_rule.match(str(case))
+                    hasAddress = self.address_compiled_rule.match(str(case))
+                    if hasName:
+                        # print hasName.group().strip()
+                        self.result['name'].append(hasName.group().strip())
+                    if hasAddress:
+                        # print hasAddress.group().strip()
+                        self.result['address'].append(hasAddress.group().strip())
+            return self.result
 
-for key, values in result.items():
-    print key + ':'
-    for value in values:
-        print value,
-    print '\n'
+
+#测试输出
+if __name__ == '__main__':
+
+    testCases = [
+        "签约仪式前，秦光荣、李纪恒、仇和等一同会见了参加签约的企业家。",
+        "王国强、高峰、汪洋、张朝阳光着头、韩寒、小四",
+        "张浩和胡健康复员回家了",
+        "王总和小丽结婚了",
+        "编剧邵钧林和稽道青说",
+        "这里有关天培的有关事迹",
+        "龚学平等领导,邓颖超生前",
+        "武胜县新学乡政府大楼门前锣鼓喧天",
+        "蓝翔给宁夏固原市彭阳县红河镇黑牛沟村捐赠了挖掘机",
+    ]
+
+
+    test = ProcessData(testCases)
+    result = test.process_data()
+
+    for key, values in result.items():
+        print key + ':'
+        for value in values:
+            print value,
+        print '\n'
 
